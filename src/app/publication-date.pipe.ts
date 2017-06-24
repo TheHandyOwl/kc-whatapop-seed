50bf569a69046a7bddaff3d2807b466139ac9b89
Carlos <thehandyowl.io@gmail.com>
2017-06-24 22:37:13 +0200

    Tuneamos la fecha del producto en la vista detalle producto

--- src/app/publication-date.pipe.ts
+++ src/app/publication-date.pipe.ts
@@ -1,3 +1,5 @@
+import { Pipe, PipeTransform} from '@angular/core';
+
 import * as moment from 'moment';
 import 'moment/locale/es';
 
@@ -12,3 +14,13 @@ import 'moment/locale/es';
 | tienes que usarla donde proceda. Haciendo                        |
 | 'moment(fecha).fromNow()' obtenemos justo lo que necesitamos.    |
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+@Pipe({
+    name: 'PublicationDate'
+})
+export class PublicationDatePipe implements PipeTransform {
+    transform(fecha: Number): String {
+        //console.log('Tipo: ', typeof(fecha), ' - valor: ', fecha);
+        return moment(fecha).fromNow();
+    }
+}
