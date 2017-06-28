3d572b367a7a20630d82923261087592750fa32a
Carlos <thehandyowl.io@gmail.com>
2017-06-28 20:34:39 +0200

    Habilitando filtros varios, botón reset y se permite presionar return en los input

--- src/app/product-filter.ts
+++ src/app/product-filter.ts
@@ -1,5 +1,15 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+| Red Wine Path                                                    |
+|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+| Añadimos los nuevos elementos de filtrado                        |
+|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 export interface ProductFilter {
     text?: string;
     category?: string;
     state?: string;
+    minPrice?: number;
+    maxPrice?: number;
+    sort?: string;
+    order?: string;
 }
